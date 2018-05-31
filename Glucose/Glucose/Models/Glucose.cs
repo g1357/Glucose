819f190ba03936eaa902a51469bdb54f0f20f049
g1357 <36661604+g1357@users.noreply.github.com>
2018-05-31 21:34:49 +0300

    Добавил модель данных

---
+++ Glucose/Glucose/Models/Glucose.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glucose.Models
+{
+    public class Glucose
+    {
+        public DateTime DateAndTime;
+        public decimal GluValue;
+        public MeasurementType MeasureType;
+        public string Remark;
+    }
+}
---
+++ Glucose/Glucose/Models/MeasurementType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glucose.Models
+{
+    public class MeasurementType
+    {
+        public string MeasureType;
+        public string MesureDescription;
+    }
+}
