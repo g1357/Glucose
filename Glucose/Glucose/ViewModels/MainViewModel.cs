215023168c508726248c8b13069eb4993fe824c8
g1357 <36661604+g1357@users.noreply.github.com>
2018-08-28 12:36:11 +0300

    Start OneDrive Access

--- Glucose/Glucose/ViewModels/MainViewModel.cs
+++ Glucose/Glucose/ViewModels/MainViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using System;
-
+using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.OneDrive.Sdk;
+using Microsoft.OneDrive.Sdk.Authentication;
 
 namespace Glucose.ViewModels
 {
@@ -9,5 +13,54 @@ namespace Glucose.ViewModels
         public MainViewModel()
         {
         }
+
+        /// <summary>
+        /// Поле: Команда проверки работы с OneDrive
+        /// </summary>
+        private ICommand _testOneDrive;
+
+        /// <summary>
+        /// Свойство: Команда проверки работы с OneDrive
+        /// </summary>
+        public ICommand TestOneDrive
+        {
+            get
+            {
+                if (_testOneDrive == null)
+                {
+                    _testOneDrive = new RelayCommand(
+                        async () =>
+                        {
+                            await DoTestOneDrive();
+                        });
+                }
+                return _testOneDrive;
+            }
+        }
+
+        // https://apps.dev.microsoft.com/
+        private static string ClientId = "0bc1162e-e51f-415e-b723-79d0ba6b0208";
+        private static string ReturnURI = "msal0bc1162e-e51f-415e-b723-79d0ba6b0208://auth";
+        private async Task DoTestOneDrive()
+        {
+            var msaAuthProvider = new MsaAuthenticationProvider(
+                ClientId,
+                ReturnURI,
+                new string[] { "onedrive.readwrite", "wl.signin" });
+            await msaAuthProvider.AuthenticateUserAsync();
+            var oneDriveClient = new OneDriveClient("https://api.onedrive.com/v1.0", msaAuthProvider);
+
+            var drive = await oneDriveClient
+                         .Drive
+                         .Request()
+                         .GetAsync();
+
+            var rootItem = await oneDriveClient
+                            .Drive
+                            .Root
+                            .Request()
+                            .GetAsync();
+        }
+
     }
 }
