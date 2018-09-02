404dea64c9ed5934a0d23ace8821587ed1c3748c
GBD <g1357@yandex.ru>
2018-09-02 14:04:18 +0300

    Добавил DataStatus

---
+++ Glucose/Glucose/Models/DataStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glucose.Models
+{
+    /// <summary>
+    /// Состояние данных в сетевом хранилище
+    /// </summary>
+    public class DataStatus
+    {
+        /// <summary>
+        /// Имя файла данных
+        /// </summary>
+        public string FileName;
+    }
+}
